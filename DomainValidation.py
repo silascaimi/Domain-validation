2931e3b6692b5baedb5cd102dd5b70cd9c4d7c5c
Silas Caimi <silascaimi@live.com>
2019-03-20 19:19:25 -0300

    Add class ClienteDb, function init and criar_schema

--- DomainValidation.py
+++ DomainValidation.py
@@ -1,5 +1,5 @@
+import sqlite3
 import io
-import re
 
 class Connect(object):
 
@@ -7,7 +7,9 @@ class Connect(object):
         try:
             self.conn = sqlite3.connect(db_name)
             self.cursor = self.conn.cursor()
+
             print("Banco:", db_name)
+
             self.cursor.execute('SELECT SQLITE_VERSION()')
             self.data = self.cursor.fetchone()
             print("SQLite version: %s" % self.data)
@@ -24,3 +26,23 @@ class Connect(object):
             self.conn.close()
             print("Conexão fechada.")
 
+class ClientesDb():
+
+    tb_name = 'clientes'
+
+    def __init__(self):
+        self.db = Connect('clientes.db')
+        self.tb_name
+
+    def criar_schema(self, schema_name='sql/clientes_schema.sql'):
+        print("Criando tabela %s ..." % self.tb_name)
+
+        try:
+            with open(schema_name, 'rt') as f:
+                schema = f.read()
+                self.db.cursor.executescript(schema)
+        except sqlite3.Error:
+            print("Aviso: A tabela %s já existe." % self.tb_name)
+            return False
+
+        print("Tabela %s criada com sucesso." % self.tb_name)
