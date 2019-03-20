d57457c7fefc0825105b9c2afffcfa2880090ec5
Silas Caimi <silascaimi@live.com>
2019-03-20 11:44:54 -0300

    Add class Conect

--- DomainValidation.py
+++ DomainValidation.py
@@ -1,6 +1,26 @@
-class DomainValidation():
+import io
+import re
 
+class Connect(object):
 
-    pass
+    def __init__(self, db_name):
+        try:
+            self.conn = sqlite3.connect(db_name)
+            self.cursor = self.conn.cursor()
+            print("Banco:", db_name)
+            self.cursor.execute('SELECT SQLITE_VERSION()')
+            self.data = self.cursor.fetchone()
+            print("SQLite version: %s" % self.data)
+        except sqlite3.Error:
+            print("Erro ao abrir banco.")
+            return False
 
+    def commit_db(self):
+        if self.conn:
+            self.conn.commit()
+
+    def close_db(self):
+        if self.conn:
+            self.conn.close()
+            print("Conexão fechada.")
 
