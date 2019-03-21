5210dd2a8b218d8f2f90e2026fff60e46d2762ba
Silas Caimi <silascaimi@live.com>
2019-03-21 09:35:37 -0300

    feat: Add features to class Clients.db
    Adiciona as funções:
    
    inserir_email
    ler_todos_clientes
    imprimir_todos_clientes
    localizar_cliente
    imprimir_cliente
    contar_cliente
    deletar

--- DomainValidation.py
+++ DomainValidation.py
@@ -46,3 +46,59 @@ class ClientesDb():
             return False
 
         print("Tabela %s criada com sucesso." % self.tb_name)
+
+    def inserir_email(self):
+        self.email = input('Email: ')
+
+        try:
+            self.db.cursor.execute("""
+            INSERT INTO clientes (email)
+            VALUES (?)
+            """, (self.email))
+
+            self.db.commit_db()
+            print("Dados inseridos com sucesso.")
+        except sqlite3.IntegrityError:
+            print("Aviso: O email deve ser único.")
+            return False
+
+    def ler_todos_clientes(self):
+        sql = 'SELECT * FROM clientes ORDER BY email'
+        r = self.db.cursor.execute(sql)
+        return r.fetchall()
+
+    def imprimir_todos_clientes(self):
+        lista = self.ler_todos_clientes()
+        print('{:>3s} {:21s}'.format('id', 'email'))
+        for c in lista:
+            print('{:3d} {:>25s}'.format(c[0], c[1]))
+
+    def localizar_cliente(self, email):
+        r = self.db.cursor.execute('SELECT * FROM clientes WHERE email = ?', (email,))
+        return r.fetchone()
+
+    def imprimir_cliente(self, email):
+        if self.localizar_cliente(email) == None:
+            print('Não existe cliente com o email informado.')
+        else:
+            print(self.localizar_cliente(email))
+
+    def contar_cliente(self):
+        r = self.db.cursor.execute('SELECT COUNT(*) FROM clientes')
+        print("Total de clientes:", r.fetchone()[0])
+
+    def deletar(self, email):
+        try:
+            c = self.localizar_cliente(email)
+
+            if c:
+                self.db.cursor.execute("""DELETE FROM clientes WHERE email = ?
+                """, (email,))
+
+                self.db.commit_db()
+                print("Registro %d excluído com sucesso." % email)
+            else:
+                print('Não existe cliente com o email informado.')
+        except e:
+            raise e
+
